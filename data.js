bb0de000ffc736c41a77a7a529a54e6ec5ebc4fd
Geekcoder127 <awesome2723@oultook.com>
2017-11-23 11:56:09 +0000

    Finished Homepage

--- data.js
+++ data.js
@@ -17,6 +17,7 @@ var data = [
   {
     name: "Hello World",
     args: [],
+    test: [],
     desc: "Simple Hello World Program",
     inst: "Write a program that returns the string 'Hello, World!'",
     diff: 1
@@ -24,6 +25,7 @@ var data = [
   {
     name: "Reverse",
     args: ["word"],
+    test: [["hello","racecar","programming"]],
     desc: "Get the Reverse of a Word",
     inst: "Write a program that returns the reverse of the given string.",
     diff: 2
